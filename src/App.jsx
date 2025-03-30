57f31abe4467291be3a465481304f234983b5c8c
GokilaSundar <gokithilaga267@gmail.com>
2025-03-30 17:07:19 +0530

    Implement frontend ui for qrcode generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
 import "./App.css";
 
+import { useState } from "react";
+
 function App() {
-  return <>Hello World</>;
+  const [text, setText] = useState("");
+
+  return (
+    <div className="container">
+      <div className="text_to_generate_qr">
+        <textarea
+          type="text"
+          value={text}
+          placeholder="Enter Text"
+          onChange={(e) => setText(e.target.value)}
+          maxLength={2096}
+          style={{ resize: "none" }}
+        />
+      </div>
+      <div className="text_qrcode">
+        {!text ? (
+          <div className="empty_text">
+            Enter Text <span>☝</span>
+          </div>
+        ) : (
+          <div className="qr_container">
+            <img src={`/api/qrcode?text=${encodeURIComponent(text)}`} />
+
+            <button
+              className="qr_generate_button"
+              onClick={() => {
+                const a = document.createElement("a");
+                a.href = `/api/qrcode?text=${encodeURIComponent(text)}`;
+                a.download = "";
+                a.click();
+              }}
+            >
+              Download
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
