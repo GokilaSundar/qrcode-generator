0189ed5cb8b4089b50624f2b1ff1bee07beca055
GokilaSundar <gokithilaga267@gmail.com>
2025-03-30 15:08:28 +0530

    Run backend along with frontend using vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import { app } from "./functions/app.js";
+
+const proxy = {
+  "/api": {},
+};
+
+function expressPlugin() {
+  return {
+    name: "express-plugin",
+    config() {
+      return {
+        server: { proxy },
+        preview: { proxy },
+      };
+    },
+    configureServer(server) {
+      server.middlewares.use(app);
+    },
+  };
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [expressPlugin(), react()],
+});
