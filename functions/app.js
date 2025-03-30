de3c53fe1c38affbcf7c41b04b33a773f181c431
GokilaSundar <gokithilaga267@gmail.com>
2025-03-30 17:05:45 +0530

    Implement qrcode api

--- functions/app.js
+++ functions/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import qrcode from "qrcode";
 
 export const app = express();
 
@@ -10,6 +11,23 @@ app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
   res.send({ message: "Hello World!" });
+});
+
+app.get("/api/qrcode", async (req, res) => {
+  try {
+    if (!req.query.text) {
+      return res.send({ message: "Text is required!" });
+    }
+
+    const qrCodeImage = await qrcode.toBuffer(req.query.text, {
+      scale: 15,
+    });
+
+    res.setHeader("Content-Type", "image/png");
+    res.send(qrCodeImage);
+  } catch (error) {
+    console.error("Failed to generate QR", error);
+  }
 });
 
 if (process.env.MONGO_URI) {
