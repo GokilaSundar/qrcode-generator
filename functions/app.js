2198385a100306e603b1eb3c1e5bf7bfe52c990b
GokilaSundar <gokithilaga267@gmail.com>
2025-04-08 23:00:29 +0530

    Use separate file for database code

--- functions/app.js
+++ functions/app.js
@@ -1,26 +1,11 @@
+import "./database.js";
+
 import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
 
 export const app = express();
-
-dotenv.config();
 
 app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
   res.send({ message: "Hello World!" });
 });
-
-if (process.env.MONGO_URI) {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-      console.log("Successfully connected to mongodb!");
-    })
-    .catch((error) => {
-      console.error("Failed to connect to mongodb!", error);
-    });
-} else {
-  console.warn("MONGO_URI is not set!");
-}
